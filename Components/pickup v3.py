29f58fbaf53f7ce08379a2698b91cb8f472da9f7
James-Wilde <99290625+James-Wilde@users.noreply.github.com>
2022-02-21 13:05:14 +1300

    Update pickup v3.py

--- Components/pickup v3.py
+++ Components/pickup v3.py
@@ -1,8 +1,10 @@
+# Customer details dictionary
 customer_details = {}
 
+# Basic instructions
 print("Please enter the pickup information")\
 
-# Customer name
+# Customer name not blank
 valid = False
 while not valid:
         customer_details['name'] = input("Please enter your name ")
@@ -13,7 +15,7 @@ while not valid:
         else:
             print("Sorry this cannot be blank")
 
-# Customer phone number
+# Customer phone number not blank
 valid = False
 while not valid:
     customer_details['phone'] = input("Please enter your phone number ")
