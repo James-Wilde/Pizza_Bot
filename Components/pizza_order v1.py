01f3fa2a970781c6be199844a9ec8607b7b86533
James-Wilde <99290625+James-Wilde@users.noreply.github.com>
2022-03-03 11:41:59 +1300

    Added pizza choice
    Pizza choice system added

--- Components/pizza_order v1.py
+++ Components/pizza_order v1.py
@@ -3,6 +3,11 @@ pizza_names = ['Margerita', 'Pepperoni', 'Hawaiian', 'Cheese', 'Italian', 'Veggi
                'BBQ Chickeh Deluxe']
 pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50 ,13.50, 13.50]
 
+#list to store ordered pizzas
+order_list = []
+#list to store pizza prices
+order_cost = []
+
 def menu():
     number_pizzas = 12
 
@@ -12,12 +17,21 @@ menu()
 # ask for total number of pizzas for order
 num_pizzas = 0
 
-num_pizzas = input("How many pizzas do you want to order? ")
+num_pizzas = int(input("How many pizzas do you want to order? "))
 
 print(num_pizzas)
 
 # choose pizza from menu
+print("Please choose your pizzas by entering the number from the menu")
+for item in range(num_pizzas):
+    while num_pizzas > 0:
+        pizza_ordered = int(input())
+        order_list.append(pizza_names[pizza_ordered])
+        order_cost.append(pizza_prices[pizza_ordered])
+        num_pizzas = num_pizzas-1
 
+print(order_list)
+print(order_cost)
 
 
 
