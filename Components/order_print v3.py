a2a04dfeb5aafb554914d4c77aa6e914d0b71f4f
James-Wilde <99290625+James-Wilde@users.noreply.github.com>
2022-03-11 14:48:20 +1300

    print order function
    added formatting to print order and added total cost. Developed into a function

--- Components/order_print v3.py
+++ Components/order_print v3.py
@@ -3,6 +3,12 @@ order_list = ['Margherita', 'Hawaiian', 'Vegan', 'BBQ Chicken Deluxe']
 #list to store pizza prices
 order_cost = [8.50, 8.50, 8.50, 13.50]
 
+cust_details = {'name': 'James', 'phone': '1234781', 'house': '93', 'street': 'Joe', 'suburb': 'Howick'}
+
+
+print(f"Customer Name: {cust_details['name']}  \nCustomer Phone: {cust_details['phone']}  \nCustomer Address: {cust_details['house']}  {cust_details['street']}  {cust_details['suburb']}")
+
+
 count = 0
 for item in order_list:
     print("Ordered: {}    Cost: ${:.2f}".format(item, order_cost[count]))
